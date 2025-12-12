fedb4d73463c737ba243ac5bde699d7c6cef36df
salmon <ellieoomf@gmail.com>
2025-12-11 16:41:36 -0800

    calculate slider background correctly

--- scripts/gather.js
+++ scripts/gather.js
@@ -5,7 +5,7 @@ const quantityslider = document.getElementById("quantityslider");
 let qtyBGPercent = 0;
 
 quantityslider.oninput = function() {
-    qtyBGPercent =  quantityslider.value; // calculate slider percent
+    qtyBGPercent = (((quantityslider.clientWidth - quantityslider.clientHeight) / quantityslider.clientWidth) * (quantityslider.value - 1) * 0.98) + (quantityslider.clientHeight / quantityslider.clientWidth * 100);
     quantityslider.style.setProperty("--slider-bg", `linear-gradient(to right, #4a8a40 ${qtyBGPercent}%, #75ab42 ${qtyBGPercent}%)`);
 
     // update number here
